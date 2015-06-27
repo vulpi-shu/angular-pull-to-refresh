8da190f481fe4457185406828b17eded373bceac
Shutov Ilya <vulpi.shu@gmail.com>
2015-06-27 14:07:21 +0600

    replace specify webkit styles to html5 fix injection for work with ffos

--- src/angular-pull-to-refresh.js
+++ src/angular-pull-to-refresh.js
@@ -17,7 +17,7 @@ angular.module('mgcrea.pullToRefresh', [])
     }
   })
 
-  .directive('pullToRefresh', function($compile, $timeout, $q, pullToRefreshConfig) {
+  .directive('pullToRefresh', ['$compile', '$timeout', '$q', 'pullToRefreshConfig', function($compile, $timeout, $q, pullToRefreshConfig) {
 
     return {
       scope: true,
@@ -27,7 +27,6 @@ angular.module('mgcrea.pullToRefresh', [])
       compile: function compile(tElement, tAttrs, transclude) {
 
         return function postLink(scope, iElement, iAttrs) {
-
           var config = angular.extend({}, pullToRefreshConfig, iAttrs);
           var scrollElement = iElement.parent();
           var ptrElement = window.ptr = iElement.children()[0];
@@ -45,109 +44,109 @@ angular.module('mgcrea.pullToRefresh', [])
           };
 
           var notIOS = function() {
-  			  	var pan = {
-  					  enabled: false,
-  					  distance: 0,
-  					  startingPositionY: 0
-  			  	};
-  			  
-  			  /**
-  				 * Determine whether pan events should apply based on scroll position on panstart
-  				 * 
-  				 * @param {object} e - Event object
-  				 */
-  				var _panStart = function(e) {
-  					pan.startingPositionY = document.body.scrollTop;
-  					if ( pan.startingPositionY === 0 ) {
-  						pan.enabled = true;
-  					}
-  				};
-
-  				/**
-  				 * Handle element on screen movement when the pandown events is firing.
-  				 * 
-  				 * @param {object} e - Event object
-  				 */
-  				var _panDown = function(e) {
-  					if ( ! pan.enabled ) {
-  						return;
-  					}
-
-  					e.preventDefault();
-  					pan.distance = e.distance / 2.5;
-
-  					_setContentPan();
-//  					_setBodyClass();
-  				};
-
-  				/**
-  				 * Handle element on screen movement when the pandown events is firing.
-  				 * 
-  				 * @param {object} e - Event object
-  				 */
-  				var _panUp = function(e) {
-  					if ( ! pan.enabled || pan.distance === 0 ) {
-  						return;
-  					}
-
-  					e.preventDefault();
-
-  					if ( pan.distance < e.distance / 2.5 ) {
-  						pan.distance = 0;
-  					} else {
-  						pan.distance = e.distance / 2.5;
-  					}
-
-  					_setContentPan();
-//  					_setBodyClass();
-  				};
-
-  				/**
-  				 * Set the CSS transform on the content element to move it on the screen.
-  				 */
-  				var _setContentPan = function() {
-  					// Use transforms to smoothly animate elements on desktop and mobile devices
-  					iElement[0].style.transform = iElement[0].style.webkitTransform = 'translateY(' + pan.distance + 'px)';
-  				};
-
-  				/**
-  				 * Determine how to animate and position elements when the panend event fires.
-  				 * 
-  				 * @param {object} e - Event object
-  				 */
-  				var _panEnd = function(e) {
-  					if ( ! pan.enabled ) {
-  						return;
-  					}
-
-  					e.preventDefault();
-  					iElement[0].style.transform = iElement[0].style.webkitTransform = '';
-
-  					pan.distance = 0;
-  					pan.enabled = false;
-  				};
-
-            	var h = new Hammer( iElement[0] );
-  			  	h.get( 'pan' ).set( { direction: Hammer.DIRECTION_VERTICAL } );
-  			  	h.on( 'panstart', _panStart );
-  			  	h.on( 'pandown', _panDown );
-  			  	h.on( 'panup', _panUp );
-  			  	h.on( 'panend', _panEnd );
+            var pan = {
+              enabled: false,
+              distance: 0,
+              startingPositionY: 0
             };
-            if (!navigator.userAgent.match(/iOS/i)) {
-            	notIOS();
+
+            /**
+             * Determine whether pan events should apply based on scroll position on panstart
+             *
+             * @param {object} e - Event object
+             */
+            var _panStart = function(e) {
+              pan.startingPositionY = document.body.scrollTop;
+              if ( pan.startingPositionY === 0 ) {
+                pan.enabled = true;
+              }
             };
-            
+
+            /**
+             * Handle element on screen movement when the pandown events is firing.
+             *
+             * @param {object} e - Event object
+             */
+            var _panDown = function(e) {
+              if ( ! pan.enabled ) {
+                return;
+              }
+
+              e.preventDefault();
+              pan.distance = e.distance / 2.5;
+
+              _setContentPan();
+            };
+
+            /**
+             * Handle element on screen movement when the pandown events is firing.
+             *
+             * @param {object} e - Event object
+             */
+            var _panUp = function(e) {
+              if ( ! pan.enabled || pan.distance === 0 ) {
+                return;
+              }
+
+              e.preventDefault();
+
+              if ( pan.distance < e.distance / 2.5 ) {
+                pan.distance = 0;
+              } else {
+                pan.distance = e.distance / 2.5;
+              }
+
+              _setContentPan();
+            };
+
+            /**
+             * Set the CSS transform on the content element to move it on the screen.
+             */
+            var _setContentPan = function() {
+              // Use transforms to smoothly animate elements on desktop and mobile devices
+              iElement[0].style.transform = iElement[0].style.transform = 'translateY(' + pan.distance + 'px)';
+            };
+
+            /**
+             * Determine how to animate and position elements when the panend event fires.
+             *
+             * @param {object} e - Event object
+             */
+            var _panEnd = function(e) {
+              if ( ! pan.enabled ) {
+                return;
+              }
+
+              e.preventDefault();
+              iElement[0].style.transform = iElement[0].style.transform = '';
+
+              pan.distance = 0;
+              pan.enabled = false;
+            };
+
+            var h = new Hammer( iElement[0] );
+            h.get( 'pan' ).set( { direction: Hammer.DIRECTION_VERTICAL } );
+            h.on( 'panstart', _panStart );
+            h.on( 'pandown', _panDown );
+            h.on( 'panup', _panUp );
+            h.on( 'panend', _panEnd );
+          };
+
+          if (!navigator.userAgent.match(/iOS/i)) {
+            notIOS();
+          };
+
 
           var shouldReload = false;
           iElement.bind('touchmove', function(ev) {
             var top = scrollElement[0].scrollTop;
             if (!navigator.userAgent.match(/iOS/i)) {
-            	var transform = iElement[0].style.transform;
-            	if (transform) {
-            	  top = -transform.replace(/translateY\(['"]?([\d.]+)px['"]?\)/i, "$1");
-            	}
+              var transform = iElement[0].style.transform;
+              if (transform) {
+                top = -transform.replace(/translateY\(['"]?([\d.]+)px['"]?\)/i, "$1");
+              }
             }
+            console.log(top < -config.treshold && !shouldReload, top, -config.treshold, !shouldReload)
             if(top < -config.treshold && !shouldReload) {
               setStatus('release');
             } else if(top > -config.treshold && shouldReload) {
@@ -157,20 +156,20 @@ angular.module('mgcrea.pullToRefresh', [])
 
           iElement.bind('touchend', function(ev) {
             if(!shouldReload) return;
-            ptrElement.style.webkitTransitionDuration = 0;
+            ptrElement.style.transitionDuration = 0;
             ptrElement.style.margin = '0 auto';
             setStatus('loading');
 
             var start = +new Date();
             $q.when(scope.$eval(iAttrs.pullToRefresh))
-            .then(function() {
-              var elapsed = +new Date() - start;
-              $timeout(function() {
-                ptrElement.style.margin = '';
-                ptrElement.style.webkitTransitionDuration = '';
-                scope.status = 'pull';
-              }, elapsed < config.debounce ? config.debounce - elapsed : 0);
-            });
+              .then(function() {
+                var elapsed = +new Date() - start;
+                $timeout(function() {
+                  ptrElement.style.margin = '';
+                  ptrElement.style.transitionDuration = '';
+                  scope.status = 'pull';
+                }, elapsed < config.debounce ? config.debounce - elapsed : 0);
+              });
           });
 
           scope.$on('$destroy', function() {
@@ -182,4 +181,4 @@ angular.module('mgcrea.pullToRefresh', [])
       }
     };
 
-  });
+  }]);
